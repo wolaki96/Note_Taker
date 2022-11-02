7a2524a84cec76bd91f2b891e36ba1d25091d959
Squiddy <wolaki96@gmail.com>
2022-11-01 21:59:13 -0400

    added in the util package correctly and figured out how to make it show upgit add -A

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -5,19 +5,20 @@ const uuid = require('../helpers/uuid');
 const db = require('../db/db.json')
 //get the route for the notes
 notes.get('/', (req, res) => {
+  console.log(req.body);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
 //post the route for the new notes
 notes.post('/', (req, res) =>{
-    // nod
-    const { noteTitle, noteText } = req.body;
+    
+    const { title, text } = req.body;
 
     if (req.body) {
       const newNote = {
         title,
         text,
-        id: uuid(),
+        
       };
   
       readAndAppend(newNote, './db/db.json');
