23857530dfc5e4add6df511bca480d3fd673c77a
Squiddy <wolaki96@gmail.com>
2022-11-01 22:37:13 -0400

    changed comments

--- Develop/helpers/Utils.js
+++ Develop/helpers/Utils.js
@@ -4,10 +4,10 @@ const util = require('util');
 // Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
 /**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
+ *  
+ *  @param {string} destination 
+ *  @param {object} content 
+ *  @returns {void} 
  */
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
@@ -15,9 +15,9 @@ const writeToFile = (destination, content) =>
   );
 /**
  *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
+ *  @param {object} content 
+ *  @param {string} file 
+ *  @returns {void} 
  */
 const readAndAppend = (content, file) => {
   fs.readFile(file, 'utf8', (err, data) => {
