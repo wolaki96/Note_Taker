af2f29923e1bcb2332c044bad856cc89c84e7373
Squiddy <wolaki96@gmail.com>
2022-11-01 20:42:15 -0400

    installed utils and uuid and moved to a new folder

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -2,5 +2,7 @@ const db = require('express').Router();
 
 // get from notes api
 fb.get('/', (req, res) =>
-  readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)))
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
+
+module.exports = db;
